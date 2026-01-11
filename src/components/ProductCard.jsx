73752bab45b4014f9a78516397e48251c9b0f0b6
Soc Basillote <dicecepe@gmail.com>
2026-01-11 20:27:38 +0800

    Delete inventory

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,28 @@
-import { useDispatch } from "react-redux";
-import { selectProduct } from "../features/products/productSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  clearSelectedProduct,
+  deleteProduct,
+  selectProduct,
+} from "../features/products/productSlice";
 
 function ProductCard({ product }) {
   const dispatch = useDispatch();
+  const selectedProduct = useSelector(
+    (state) => state.products.selectedProduct
+  );
+
+  const handleDelete = () => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delte this product?"
+    );
+    if (!confirmDelete) return;
+
+    if (selectedProduct?._id === product._id) {
+      dispatch(clearSelectedProduct());
+    }
+
+    dispatch(deleteProduct(product._id));
+  };
   return (
     <div className="bg-white rounded-lg shadow p-4">
       <div className="h-40 bg-gray-100 rounded mb-3 overflow-hidden">
@@ -32,6 +52,12 @@ function ProductCard({ product }) {
         className="mt-3 w-full border border-blue-600 text-blue-600 rounded py-1 hover:bg-blue-50"
       >
         Edit
+      </button>
+      <button
+        onClick={handleDelete}
+        className="flex-1 border border-red-600 text-red-600 rounded py-1 hover:bg-red-50"
+      >
+        Delete
       </button>
     </div>
   );
